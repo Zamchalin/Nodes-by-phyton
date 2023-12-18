664cfddd12a4e073b10d82022ccf8510eaad8cb8
Michael <zamchalin@mail.ru>
2023-12-18 12:23:04 +0300

    Added DZ

--- NodesForUser.py
+++ NodesForUser.py
@@ -3,7 +3,7 @@ import datetime
 
 def add_note():
     title = input("Введите заголовок заметки: ")
-    msg = input("Введите тело заметки: ")
+    msg = input("Введите содержание  заметки: ")
     note_id = len(notes) + 1
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     notes.append({"id": note_id, "title": title, "msg": msg, "date": date})
@@ -25,3 +25,57 @@ def read_notes():
                 print(f"{note['id']}. {note['title']} ({note['date']})\n{note['msg']}\n")
         else:
             print("Заметок с указанной датой нет")
+
+def edit_note():
+    note_id = int(input("Введите id заметки которую хотите изменить: "))
+    for note in notes:
+        if note["id"] == note_id:
+            title = input(f"Введите новый заголовок заметки ({note['title']}): ")
+            msg = input(f"Введите новое содержание заметки ({note['msg']}): ")
+            if title:
+                note["title"] = title
+            if msg:
+                note["msg"] = msg
+            note["date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            save_notes()
+            print("Заметка успешно изменена")
+            return
+    print(f"Заметка с id {note_id} не найдена")
+
+def delete_note():
+    note_id = int(input("Введите id заметки для удаления: "))
+    for note in notes:
+        if note["id"] == note_id:
+            notes.remove(note)
+            save_notes()
+            print("Заметка удалена")
+            return
+    print(f"Заметка с id {note_id} не найдена")
+
+def save_notes():
+    with open("notes.json", "w") as f:
+        json.dump(notes, f)      
+
+def load_notes():
+    try:
+        with open("notes.json", "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return []
+
+if __name__ == "__main__":
+    notes = load_notes()
+    while True:
+        command = input("Введите команду:\n1.Добавить\n2.Прочитать\n3.Редактировать\n4.Удалить\n5.Выход\n ")
+        if command == "1":
+            add_note()
+        elif command == "2":
+            read_notes()
+        elif command == "3":
+            edit_note()
+        elif command == "4":
+            delete_note()
+        elif command == "5":
+            break
+        else:
+            print("Неверная команда")
