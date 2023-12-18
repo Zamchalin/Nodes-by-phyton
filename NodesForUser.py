575f99f64b7255c8cb20e9e5eee9920111c9d4da
Michael <zamchalin@mail.ru>
2023-12-18 12:19:23 +0300

    Added def for read note

--- NodesForUser.py
+++ NodesForUser.py
@@ -9,3 +9,19 @@ def add_note():
     notes.append({"id": note_id, "title": title, "msg": msg, "date": date})
     save_notes()
     print("Заметка успешно сохранена")
+
+def read_notes():
+    filter_date = input("Введите дату для фильтрации (в формате ГГГГ-ММ-ДД): ")
+    if not notes:
+        print("Заметок нет")
+        return
+    if not filter_date:
+        for note in notes:
+            print(f"{note['id']}. {note['title']} ({note['date']})\n{note['msg']}\n")
+    else:
+        filtered_notes = [note for note in notes if note["date"].startswith(filter_date)]
+        if filtered_notes:
+            for note in filtered_notes:
+                print(f"{note['id']}. {note['title']} ({note['date']})\n{note['msg']}\n")
+        else:
+            print("Заметок с указанной датой нет")
